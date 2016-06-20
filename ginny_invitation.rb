8b4a4e1e1de64c1ea30981c865877045b1e9d198
Grace Foley <gfoley18@spenceschool.org>
2016-06-20 16:28:16 +0000

    Done.

--- ginny_invitation.rb
+++ ginny_invitation.rb
@@ -1,1 +1,8 @@
-ginny_invitation.rb
+name="Ginny Weasley"
+day="Sunday"
+date="17th"
+year="1998"
+invitation = "The family of #{name} proudly invite you
+to their graduation commencement on #{day} the #{date} of May #{year}.
+Festivities will be held at The Burrow. See you then!"
+puts invitation
--- invitation.rb
+++ invitation.rb
@@ -1,1 +1,7 @@
 #Code Here!
+percyInvitation = "The family of Percy Weasley proudly invite
+you to their graduation commencement on Saturday the 22nd of May 1993.
+Festivities will be held at The Burrow. See you then!"
+ronInvitation=percyInvitation.gsub("Saturday 22nd of May 1993",
+"Sunday the 18th of May 1997").gsub("Percy Weasley","Ron Weasley")
+puts ronInvitation
